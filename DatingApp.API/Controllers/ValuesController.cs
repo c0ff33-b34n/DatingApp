4457ce4938f160a102cc0cce6242504a4ee97b50
Stuart Bowes <stuartbowes2000@gmail.com>
2020-01-14 14:53:48 +0000

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+        // GET api/values
+        // [HttpGet]
+        // public IActionResult GetValues()
+        // {
+        //     var values = _context.Values.ToList(); // Accesses database, retrieves Values, stores in values as a list.
+
+        //     return Ok(values);
+        // } THIS method is synchronous, so not really scalable as it will block the thread until the database query
+        // has completed and returned values, so we need to make something scalable, i.e. asynchronous.
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync(); // Accesses database, retrieves Values, stores in values as a list.
+
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id ); // FirstOrDefault will return value or null if not found
+                                                            // otherwise using First (might throw an exception if not found.)
+                                                            // x represents the value being returned
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
